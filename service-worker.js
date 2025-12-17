92145a981f2e970cc7cd633574b416d5e08367bf
98cookqie <kathyhuang0615@gmail.com>
2025-12-17 23:33:53 +0800

    Update cached HTML file in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'seoul-trip-cache-v4';
 const urlsToCache = [
-    './Seoul_Travel_App_Muji_V3.html',
+    './S_index.html',
     './manifest.json',
     './service-worker.js'
 ];
